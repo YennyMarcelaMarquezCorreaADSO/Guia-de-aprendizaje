512cc1373eb338c23685221816bbcec3783523bc
YennyMarcelaMárquezCorreaADSO <yennycorrea0911@gmail.com>
2022-12-06 10:08:51 -0500

    Evidencia 5, Taller 1

--- Evidencias, Taller 1/main.js
+++ Evidencias, Taller 1/main.js
@@ -1,23 +1,23 @@
 addEventListener("DOMContentLoaded", (e)=>{
     let form=document.querySelector('#input')
 
-
+    let conta = 1
     form.addEventListener("submit",(e)=>{
-        e.preventDefault();       
-        let dataInput = Object.fromEntries(new FormData(e.target))
-        let centigrados = Number (dataInput.centigrados)
-        let fahrenheit= (centigrados*9/5)+32
-
-
-
+        e.preventDefault();    
+        if(conta <= 40){
+            let dataInput = Object.fromEntries(new FormData(e.target))
+            let fech = dataInput.fecha
+            let plans = dataInput.planes
             let tabla=document.querySelector("tbody");
             tabla.insertAdjacentHTML("beforeend",`
                 <tr>
-                    <td> ${centigrados}  </td>
-                    <td> ${fahrenheit} </td>
+                    <td> ${conta} </td>
+                    <td> ${fech}  </td>
+                    <td> ${plans} </td>
                 </tr>
                 `)
-     })        
+            conta++
+        }
 
-    
+     })         
 })
